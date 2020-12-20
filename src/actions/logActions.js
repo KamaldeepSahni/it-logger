f26d13ec2e5465e1434777c423007616998ac1f0
Kamaldeep <ksahni687@gmail.com>
2020-12-20 22:52:31 +0530

    Update Func

--- src/actions/logActions.js
+++ src/actions/logActions.js
@@ -4,6 +4,9 @@ import {
   LOGS_ERROR,
   ADD_LOG,
   DELETE_LOG,
+  SET_CURRENT,
+  CLEAR_CURRENT,
+  UPDATE_LOG,
 } from './types';
 
 // Get logs from server
@@ -69,6 +72,47 @@ export const deleteLog = id => async dispatch => {
       payload: err.response.data,
     });
   }
+};
+
+// Update Log
+export const updateLog = log => async dispatch => {
+  try {
+    setLoading();
+
+    const res = await fetch(`/logs/${log.id}`, {
+      method: 'PUT',
+      body: JSON.stringify(log),
+      headers: {
+        'Content-type': 'application/json',
+      },
+    });
+    const data = await res.json();
+
+    dispatch({
+      type: UPDATE_LOG,
+      payload: data,
+    });
+  } catch (err) {
+    dispatch({
+      type: LOGS_ERROR,
+      payload: err.response.data,
+    });
+  }
+};
+
+// Set Current Log
+export const setCurrent = log => {
+  return {
+    type: SET_CURRENT,
+    payload: log,
+  };
+};
+
+// Clear Current Log
+export const clearCurrent = () => {
+  return {
+    type: CLEAR_CURRENT,
+  };
 };
 
 // set loading to true
--- src/components/logs/LogItem.js
+++ src/components/logs/LogItem.js
@@ -4,14 +4,11 @@ import Moment from 'react-moment';
 import PropTypes from 'prop-types';
 import M from 'materialize-css/dist/js/materialize.min.js';
 
-import { deleteLog } from '../../actions/logActions';
+import { deleteLog, setCurrent } from '../../actions/logActions';
 
-const LogItem = ({
-  log: { id, message, tech, attention, date },
-  deleteLog,
-}) => {
+const LogItem = ({ log, deleteLog, setCurrent }) => {
   const onDelete = () => {
-    deleteLog(id);
+    deleteLog(log.id);
 
     M.toast({ html: 'Log Deleted' });
   };
@@ -21,15 +18,18 @@ const LogItem = ({
       <div>
         <a
           href="#edit-log-modal"
-          className={`modal-trigger ${attention ? 'red-text' : 'blue-text'}`}
+          className={`modal-trigger ${
+            log.attention ? 'red-text' : 'blue-text'
+          }`}
+          onClick={() => setCurrent(log)}
         >
-          {message}
+          {log.message}
         </a>
         <br />
         <span className="grey-text">
-          <span className="black-text">ID #{id}</span> last updated by{' '}
-          <span className="black-text">{tech}</span> on{' '}
-          <Moment format="MMMM Do YYYY, h:mm:ss a">{date}</Moment>
+          <span className="black-text">ID #{log.id}</span> last updated by{' '}
+          <span className="black-text">{log.tech}</span> on{' '}
+          <Moment format="MMMM Do YYYY, h:mm:ss a">{log.date}</Moment>
         </span>
         <a href="#!" onClick={onDelete} className="secondary-content">
           <i className="material-icons grey-text">delete</i>
@@ -42,6 +42,7 @@ const LogItem = ({
 LogItem.propTypes = {
   log: PropTypes.object.isRequired,
   deleteLog: PropTypes.func.isRequired,
+  setCurrent: PropTypes.func.isRequired,
 };
 
-export default connect(null, { deleteLog })(LogItem);
+export default connect(null, { deleteLog, setCurrent })(LogItem);
