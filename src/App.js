09e485c1702849059daa0973f02b9ab259ee3fdb
Kamaldeep <ksahni687@gmail.com>
2020-12-20 17:02:04 +0530

    Basic fron End

--- src/App.js
+++ src/App.js
@@ -3,13 +3,35 @@ import 'materialize-css/dist/css/materialize.min.css';
 import M from 'materialize-css/dist/js/materialize.min.js';
 import './App.css';
 
+import SearchBar from './components/layout/SearchBar';
+import AddBtn from './components/layout/AddBtn';
+
+import Logs from './components/logs/Logs';
+import AddLogModal from './components/logs/AddLogModal';
+import EditLogModal from './components/logs/EditLogModal';
+
+import AddTechModal from './components/techs/AddTechModal';
+import TechListModal from './components/techs/TechListModal';
+
 const App = () => {
   useEffect(() => {
     // Init Materialize
     M.AutoInit();
   });
 
-  return <div className="App">My App</div>;
+  return (
+    <>
+      <SearchBar />
+      <div className="container">
+        <AddBtn />
+        <AddLogModal />
+        <EditLogModal />
+        <AddTechModal />
+        <TechListModal />
+        <Logs />
+      </div>
+    </>
+  );
 };
 
 export default App;
